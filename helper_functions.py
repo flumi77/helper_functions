061dc0d1efd8fd174cb12804fdce62224557a9fc
flumi77 <michael.fluck.de@gmail.com>
2025-12-04 21:18:16 +0100

    Implement timer for training duration measurement
    Added timer functionality to measure training duration.

--- helper_functions.py
+++ helper_functions.py
@@ -4,7 +4,14 @@ import numpy as np
 def say_hello():
   print("Hello!")
 
-'''Use default_timer from timeit'''
+'''Use default_timer from timeit to measure start and end time
+from timeit import default_timer as timer
+
+start = timer()
+some code ...
+end = timer()
+print_train_time(start, end, device)
+'''
 def print_train_time(start: float, end: float, device: torch.device = None):
     total_time = end - start
     print(f"Train time on {device}: {total_time:.3f} seconds")
