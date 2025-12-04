23b61240fd6617ad60091278a2a5e1a5bc13d32b
flumi77 <michael.fluck.de@gmail.com>
2025-12-04 21:15:17 +0100

    Add print_train_time function for training duration

--- helper_functions.py
+++ helper_functions.py
@@ -3,3 +3,9 @@ import numpy as np
 
 def say_hello():
   print("Hello!")
+
+'''Use default_timer from timeit'''
+def print_train_time(start: float, end: float, device: torch.device = None):
+    total_time = end - start
+    print(f"Train time on {device}: {total_time:.3f} seconds")
+    return total_time
